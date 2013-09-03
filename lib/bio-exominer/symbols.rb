2606219fa11616a98ca3d83ad75fbc53255dac1e
Pjotr Prins <pjotr.public01@thebird.nl>
2013-09-03 16:15:41 +0200

    Generate output

--- lib/bio-exominer/symbols.rb
+++ lib/bio-exominer/symbols.rb
@@ -3,6 +3,16 @@ module BioExominer
   module Symbols
 
     def Symbols::each(fn) 
+      File.open(fn).each_line do | line |
+        symbol,aliases,descr = line.split(/\t/)
+        aliases = 
+          if aliases == 'NA'
+            nil
+          else
+            aliases.split(/\|/)
+          end
+        yield symbol,aliases,descr.strip
+      end
     end
   end
 
